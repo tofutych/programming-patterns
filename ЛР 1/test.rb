82d2528e272b0b3203c68b7f33ff89b3d0d7119c
David <martirosov.davidka@mail.ru>
2021-02-24 15:35:07 +0300

    баловство

---
+++ ЛР 1/test.rb
@@ -0,0 +1,23 @@
+class Sh
+    def look
+        "iden"
+    end
+
+    def chains
+        handle.this
+        .long
+        .chain
+    end
+
+    def i am, great
+        am * great
+    end
+    
+    module Test
+    end
+end
+
+class am
+    def aaa
+        pass
+    end
