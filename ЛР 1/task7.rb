ff4cca8439a439c51404384eb04724e9f0c3cb70
David <martirosov.davidka@mail.ru>
2021-02-27 02:23:42 +0300

    method 2

---
+++ ЛР 1/task7.rb
@@ -0,0 +1,14 @@
+def method_2(num)
+    num = num.to_i
+    max_digit = -1
+    while num > 0
+        digit = num % 10
+        if digit > max_digit && digit % 3 != 0 then
+            max_digit = digit
+        end
+        num /= 10
+    end
+    if max_digit == -1 then return "Такой цифры нет" else return max_digit end
+end
+
+puts method_2("36901234598")
