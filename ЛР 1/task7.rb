eb83ae1edb77d3386eb66a140e300e4f9183d02a
David <martirosov.davidka@mail.ru>
2021-03-02 16:22:31 +0300

    method1

--- ЛР 1/task7.rb
+++ ЛР 1/task7.rb
@@ -1,3 +1,26 @@
+def method_1(num)
+    num = num.to_i
+    counter = 0
+    for i in 2...num
+        if i.even?
+            acc = 0
+            for j in 2..i
+                if num % j == 0 && i % j == 0
+                    acc += 1
+                end
+            end
+            if acc != 0
+                counter += 1
+            end
+        end
+    end
+    puts counter
+end
+
+method_1(35)  # -> 5
+method_1(10)  # -> 4
+
+
 def method_2(num)
     num = num.to_i
     max_digit = -1
@@ -11,4 +34,10 @@ def method_2(num)
     if max_digit == -1 then return "Такой цифры нет" else return max_digit end
 end
 
-puts method_2("3690123456789")
+puts method_2("123")  # -> 2
+puts method_2("987654321")  # -> 8
+
+
+
+def method_3()
+end
