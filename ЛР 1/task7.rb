2e0ccc2419a3aa7ca68798519e8436dab5faca52
David <martirosov.davidka@mail.ru>
2021-03-02 17:32:09 +0300

    method3

--- ЛР 1/task7.rb
+++ ЛР 1/task7.rb
@@ -1,6 +1,7 @@
 def method_1(num)
     num = num.to_i
     counter = 0
+
     for i in 2...num
         if i.even?
             acc = 0
@@ -15,6 +16,7 @@ def method_1(num)
             end
         end
     end
+
     puts counter
 end
 
@@ -25,6 +27,7 @@ method_1(10)  # -> 4
 def method_2(num)
     num = num.to_i
     max_digit = -1
+
     while num > 0
         digit = num % 10
         if digit > max_digit && digit % 3 != 0 then
@@ -32,13 +35,45 @@ def method_2(num)
         end
         num /= 10
     end
+
     if max_digit == -1 then return "Такой цифры нет" else return max_digit end
 end
 
-# puts method_2("123")  # -> 2
-# puts method_2("987654321")  # -> 8
+puts method_2("123")  # -> 2
+puts method_2("987654321")  # -> 8
 
 
 
-def method_3()
+def method_3(num)
+    num = num.to_i
+    sum = 0
+    smallest_divisor = nil
+    max_num = nil
+
+    for i in 2..num
+        if num % i == 0
+            smallest_divisor = i
+            break
+        end
+    end
+
+    (num - 1).downto(2) do |i|
+        for j in 2..i
+            if num % j == 0 && i % j == 0 && i % smallest_divisor != 0
+                max_num = i
+                break
+            end
+        end
+    end
+    
+    while num > 0
+        if num % 10 < 5
+            sum += num % 10
+        end
+        num /= 10
+    end
+    puts max_num * sum
 end
+
+method_3(123)  # -> 245
+method_3(725)  # -> 245
