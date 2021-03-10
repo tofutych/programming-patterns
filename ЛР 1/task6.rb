3aa5cdd101b113dc01a72c560ede5ec7dd260ce0
Tofut <martirosov.davidka@mail.ru>
2021-03-10 17:26:39 +0300

    individual task 8

--- ЛР 1/task6.rb
+++ ЛР 1/task6.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 def sum(num)
     acc = 0
 
@@ -47,6 +49,9 @@ end
 
 
 def not_coprime?(num1, num2)
+    if num1 > num2
+        num1, num2 = num2, num1
+    end
     for i in 2..num2
         if num1 % i == 0 && num2 % i == 0
             return true
@@ -76,7 +81,7 @@ def sum_of_digits_less_than_5(num)
         num /= 10
     end
 
-    return s
+    s
 end
 
 
@@ -90,11 +95,8 @@ def method_1(num)
         end
     end
 
-    return counter
+    counter
 end
-
-puts method_1(35)  # -> 5
-puts method_1(10)  # -> 4
 
 
 def method_2(num)
@@ -114,10 +116,6 @@ def method_2(num)
 end
 
 
-puts method_2("123")        # -> 2
-puts method_2("987654321")  # -> 8
-
-
 def method_3(num)
     num = num.to_i
     smallest_divisor = get_smallest_divisor(num)
@@ -129,13 +127,100 @@ def method_3(num)
 end
 
 
-puts method_3("123")  # -> 492
-puts method_3("725")  # -> 1392
+def selector()
+    for argv in ARGV
+        case argv
+        when "1"
+            print "Вы выбрали sum(num)\n\tВведите num: "
+            num = STDIN.gets.chomp
+            puts "\tРезультат: #{sum(num)}".blue
+            puts 
+        when "2"
+            print "Вы выбрали product(num)\n\tВведите num: "
+            num = STDIN.gets.chomp
+            puts "\tРезультат: #{product(num)}".blue
+            puts
+        when "3"
+            print "Вы выбрали min_digit(num)\n\tВведите num: "
+            num = STDIN.gets.chomp
+            puts "\tРезультат: #{min_digit(num)}".blue
+            puts
+        when "4"
+            print "Вы выбрали max_digit(num)\n\tВведите num: "
+            num = STDIN.gets.chomp
+            puts "\tРезультат: #{max_digit(num)}".blue
+            puts
+        when "5"
+            print "Вы выбрали not_coprime?(num1, num2)\n\tВведите num1, num2(через пробел): "
+            nums = STDIN.gets.chomp.split()
+            num1 = nums[0].to_i
+            num2 = nums[1].to_i
+            puts "\tРезультат: #{not_coprime?(num1, num2)}".blue
+            puts
+        when "6"
+            print "Вы выбрали get_smallest_divisor(num)\n\tВведите num: "
+            num = STDIN.gets.chomp.to_i
+            puts "\tРезультат: #{smallest_divisor(num)}".blue
+            puts
+        when "7"
+            print "Вы выбрали sum_of_digits_less_than_5(num)\n\tВведите num: "
+            num = STDIN.gets.chomp.to_i
+            puts "\tРезультат: #{sum_of_digits_less_than_5(num)}".blue
+            puts
+        when "8"
+            print "Вы выбрали method1(num)\n\tВведите num: "
+            num = STDIN.gets.chomp
+            puts "\tРезультат: #{method_1(num)}".blue
+            puts
+        when "9"
+            print "Вы выбрали method2(num)\n\tВведите num: "
+            num = STDIN.gets.chomp
+            puts "\tРезультат: #{method_2(num)}".blue
+            puts
+        when "10"
+            print "Вы выбрали method3(num)\n\tВведите num: "
+            num = STDIN.gets.chomp
+            puts "\tРезультат: #{method_3(num)}".blue
+            puts
+        when "-h"
+            h()
+        else
+            puts "Такого метода нет.".red
+            puts
+            h()
+        end
+    end
+end
 
-num = ARGV[0]  # ruby task6.rb 123
+
+def h()
+    puts "Всего доступно 10 методов.\nЧтобы вызвать какой-либо из них, в параметрах запуска укажите " + "номер метода и необходимые значения.\n".red
+    puts "\t1.  sum(num) — сумма цифр числа num"
+    puts "\t2.  product(num) — произведение цифр числа num"
+    puts "\t3.  min_digit(num) — минимальная цифра числа num"
+    puts "\t4.  max_digit(num) — максимальная цифра числа num"
+    puts "\t5.  not_coprime?(num1, num2) — не взаимная простота чисел num1 и num2"
+    puts "\t6.  get_smallest_divisor(num) — минимальный делитель числа num"
+    puts "\t7.  sum_of_digits_less_than_5(num) — сумма чисел числа, которые меньше 5"
+    puts "\t8.  method1(num) — Количество четных чисел, не взаимно простых с числом num".blue  # 35 - > 5, 10 -> 4
+    puts "\t9.  method2(num) — Максимальная цифру числа num, не делящуюся на 3.".blue  # 123 -> 2, 987654321 -> 3
+    puts "\t10. method3(num) — Поизведение максимального числа, не взаимно простого с num, не делящегося на наименьший делитель исходного числа num, и суммы цифр числа num, меньших 5.\n".blue  # 123 -> 492, 725 -> 1392
+end
 
 
-puts sum(num)        # -> 6
-puts product(num)    # -> 6
-puts min_digit(num)  # -> 1
-puts max_digit(num)  # -> 3
+def main
+    if ARGV.empty?
+        puts "Используйте параметры запуска для работы с программой!".red
+        puts
+        h()
+    end
+
+    if ARGV[0] == '-h' && ARGV.size == 1
+        h()
+    else
+        selector()
+    end
+end
+
+
+main()
