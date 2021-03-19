0a194cac308fd6a878926359a0cd2d6ba596f9c2
Tofut <martirosov.davidka@mail.ru>
2021-03-19 04:41:49 +0300

    12

---
+++ ЛР 2/task12.rb
@@ -0,0 +1,8 @@
+def task12(path)
+    data = File.open(path).readlines.map(&:chomp)
+    return data
+    return data.sort_by(&:length)
+end
+
+
+print task12("task12.txt")  # ["qwerty", "qwert", "qwer", "qwe", "qw", "q"]  ——>  ["q", "qw", "qwe", "qwer", "qwert", "qwerty"]
