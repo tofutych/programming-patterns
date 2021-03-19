6ac69fb63fedbfea2c8d52b7b0c7e0974f02b679
Tofut <martirosov.davidka@mail.ru>
2021-03-19 04:32:02 +0300

    menu

--- ЛР 2/task10.rb
+++ ЛР 2/task10.rb
@@ -17,3 +17,34 @@ def task15(str)
     return str.scan(/\d+/).map(&:to_i).select{|n| n > 5}.size
 end
 
+
+def input_string()
+    print "Введите строку: "
+    return STDIN.gets.chomp
+end
+
+
+def main()
+    puts "Выбирай ченить:"
+    puts "\t1. Колво чисел строки < 5"
+    puts "\t2. Незадействованная латиница"
+    puts "\t3. Колво чисел строки > 5"
+    answer = STDIN.gets.chomp
+
+    case answer
+    when "1"
+        str = input_string()
+        return task4(str)
+    when "2"
+        str = input_string()
+        return task11(str)
+    when "3"
+        str = input_string()
+        return task15(str)
+    else
+        return "Чето не то выбрал, друг."
+    end
+end
+
+
+puts main()
