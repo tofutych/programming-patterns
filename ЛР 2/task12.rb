461ed12579e2becb5b8d514d6647e62fb9520e28
Tofut <martirosov.davidka@mail.ru>
2021-03-19 04:48:49 +0300

    13

--- ЛР 2/task12.rb
+++ ЛР 2/task12.rb
@@ -4,4 +4,11 @@ def task12(path)
 end
 
 
+def task13(path)
+    data = File.open(path).readlines.map(&:chomp)
+    return data.sort_by{|x| x.split.count}
+end
+
+
 print task12("task12.txt")  # ["qwe qwe qwe", "qweeeeeeee", "qw qw"]  ——>  ["qw qw", "qweeeeeeee", "qwe qwe qwe"]
+print task13("task12.txt")  # ["qwe qwe qwe", "qweeeeeeee", "qw qw"]  ——>  ["qweeeeeeee", "qw qw", "qwe qwe qwe"]
