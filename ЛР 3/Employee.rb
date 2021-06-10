6b8576eaca9e343ae7d5482aada31a631bcead8c
Tofut <martirosov.davidka@mail.ru>
2021-06-10 15:01:49 +0300

    ru phone validator

--- ЛР 3/Employee.rb
+++ ЛР 3/Employee.rb
@@ -4,7 +4,7 @@ class Employee
     def initialize(fio, bday_year, phone_number, address, email, passport, speciality, work_exp, prev_wp = nil, prev_speciality = nil, prev_salary = nil)
         self.fio= fio
         self.bday_year= bday_year
-        self.phone_number= phone_number
+        self.phone_number= Employee.correct_phone_number(phone_number)
         self.address= address
         self.email= email
         self.passport= passport
@@ -29,10 +29,24 @@ class Employee
         s
     end
 
-    def self.is_russian_phone_number?(phone)
-        if phone.match /^((\+7|7|8)+([0-9]){10})$/ == nil
-            false
+    def self.is_ru_phone_number?(phone)
+        if (/^((\+7|7|8)+([0-9]){10})$/).match(phone) == nil
+            return false
         end
         true
     end
+    
+
+    def self.correct_phone_number(phone)
+        if self.is_ru_phone_number?(phone)
+            nums = phone.chars.map{|symb| symb if symb =~ /[0-9]/}.join
+            return "7-" + nums[1..3] + "-" + nums[4..]
+        else
+            raise "Invalid phone number."
+        end
+    end
+
 end
+
+emp2 = Employee.new("Ivanov Ivan Ivanovich", 2000, "89001234567", "Ulitsya Pushkina Dom Kolotushkina", "ivanov@ivan.ivanovich", "12 34 567890", "Waiter", 5, "Moscow", "Janitor", 10000)
+puts emp2
